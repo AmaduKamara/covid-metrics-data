9702fa16c5c893f36de0fafe50506cf1ae788729
Amkam <amaduskamara@gmail.com>
2022-01-31 19:14:52 +0000

    Add pages components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
-import Header from "./components/Header";
+import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
---
+++ src/pages/Details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Details = () => {
+  return (
+    <div>
+      <h1>Details Page</h1>
+    </div>
+  );
+};
+
+export default Details;
