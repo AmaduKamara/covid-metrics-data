f596311f41540aeff514df554d56b8290c89ef46
Amkam <amaduskamara@gmail.com>
2022-01-31 19:05:46 +0000

    Add prettier config file and Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>COVID-19 Metrics Data</h1>
+      <Header />
     </div>
   );
 }
