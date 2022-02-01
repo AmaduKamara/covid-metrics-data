ab28deea5b583402de8bc45843faf7553bef2ed6
Amkam <amaduskamara@gmail.com>
2022-02-01 19:52:19 +0000

    Add hero section's height

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => (
-  <div className="h-72 flex items-center justify-center hero">
+  <div className="h-80 flex items-center justify-center hero">
     <div className="w-2/3 md:w-3/4">
       <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-7xl text-white mb-2">COVOID-19 Metrics</h1>
       <p className="text-xs sm:text-lg font-thin text-white">
