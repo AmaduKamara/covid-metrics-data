2fe1b9ca789c828de57c37ed649d6d54d663016e
Amkam <amaduskamara@gmail.com>
2022-02-01 14:58:47 +0000

    Install redux, react-redux and created a store directory

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
   <div className="h-72 flex items-center justify-center hero">
     <div className="w-1/3">
-      <h1 className="text-7xl text-white mb-4">COVOIID-19 Metrics</h1>
+      <h1 className="text-7xl text-white mb-4">COVOID-19 Metrics</h1>
       <p className="text-lg font-thin text-white">
         Get to know the latest updates on current COVID-19 metrics around the
         world
