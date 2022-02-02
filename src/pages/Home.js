537033d7abbee42c709ef060862024e953b28c05
Amkam <amaduskamara@gmail.com>
2022-02-02 18:44:19 +0000

    Add search filter functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCovid } from '../store/covid/covid';
 import Country from '../components/Country';
 
 const Home = () => {
+  const [searchText, setSearchText] = useState('');
+
   const covidData = useSelector((state) => state.covidReducer.covidData);
   const dispatch = useDispatch();
 
@@ -11,12 +13,20 @@ const Home = () => {
     dispatch(fetchCovid());
   }, []);
 
+  const searchFilter = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const filtered = covidData.filter((item) => Object.keys(item).some((key) => item[key]
+    .toString()
+    .toLowerCase()
+    .includes(searchText.toLocaleLowerCase())));
+
   return (
     <div>
-      {/* <Hero /> */}
       <div className="h-56 flex items-center justify-center hero sticky top-0">
         <div className="w-2/3 md:w-3/4">
-          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl text-white mb-2">
+          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl text-white">
             COVOID-19 Metrics
           </h1>
           <p className="text-xs sm:text-lg font-thin text-white">
@@ -27,11 +37,13 @@ const Home = () => {
             type="text"
             placeholder="Search by country name"
             className="py-3 px-5 mt-3 focus:outline-pink-500 rounded-full w-2/4 md:3/4 lg:w-2/5"
+            value={searchText}
+            onChange={searchFilter}
           />
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
-        {covidData.map((country) => (
+        {filtered.map((country) => (
           <Country
             key={country.ID}
             country={country.Country}
