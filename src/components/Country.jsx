fe9dc7be36546f9c4bffd0e01a0b6e11e29c5bc1
Amkam <amaduskamara@gmail.com>
2022-02-02 17:38:47 +0000

    Add useNavigate for navigating to the details page

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,17 +1,30 @@
 import { PropTypes } from 'prop-types';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import { BsArrowRightCircle } from 'react-icons/bs';
 
-const Country = ({ country, TotalConfirmed }) => (
-  <div className="border border-pink-400 p-5 country">
-    <div className="flex justify-end items-end">
-      <BsArrowRightCircle className="text-white text-2xl cursor-pointer" />
+const Country = ({ country, TotalConfirmed }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="border border-pink-400 p-5 country">
+      <div className="flex justify-end items-end">
+        <BsArrowRightCircle
+          onClick={() => navigate('/details')}
+          className="text-white text-2xl cursor-pointer"
+        />
+      </div>
+      <div className="flex flex-col justify-end items-end mt-5">
+        <h1 className="text-xs sm:text-lg md:text-xl lg:text-2xln font-semibold uppercase text-white">
+          {country}
+        </h1>
+        <p className="text-white text-xs sm:text-sm md:text-lg mt-1">
+          {TotalConfirmed}
+        </p>
+      </div>
     </div>
-    <div className="flex flex-col justify-end items-end mt-5">
-      <h1 className="text-xs sm:text-lg md:text-xl lg:text-2xln font-semibold uppercase text-white">{country}</h1>
-      <p className="text-white text-xs sm:text-sm md:text-lg mt-1">{TotalConfirmed}</p>
-    </div>
-  </div>
-);
+  );
+};
 
 Country.propTypes = {
   country: PropTypes.string.isRequired,
