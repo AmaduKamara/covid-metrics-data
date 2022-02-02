4dba69826702c20cd233df374ab3eebe22bec4e2
Amkam <amaduskamara@gmail.com>
2022-02-02 17:57:39 +0000

    Add center style in app and removed the old css App styles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-center">
       <Hero />
       <Routes>
         <Route path="/" element={<Home />} />
