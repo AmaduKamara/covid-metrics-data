a4eb25b4bbc0a3bdae1ba022f8918c0af980a317
Amkam <amaduskamara@gmail.com>
2022-02-02 18:10:13 +0000

    Moved Hero section code to Home page and deleted it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
-import Hero from './components/Hero';
 import Home from './pages/Home';
 import Details from './pages/Details';
 import Footer from './components/Footer';
@@ -9,7 +8,6 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="text-center">
-      <Hero />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Details />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,6 +13,23 @@ const Home = () => {
 
   return (
     <div>
+      {/* <Hero /> */}
+      <div className="h-56 flex items-center justify-center hero sticky top-0">
+        <div className="w-2/3 md:w-3/4">
+          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl text-white mb-2">
+            COVOID-19 Metrics
+          </h1>
+          <p className="text-xs sm:text-lg font-thin text-white">
+            Get to know the latest updates on current COVID-19 metrics around
+            the world
+          </p>
+          <input
+            type="text"
+            placeholder="Search by country name"
+            className="py-3 px-5 mt-3 focus:outline-pink-500 rounded-full w-2/4 md:3/4 lg:w-2/5"
+          />
+        </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6">
         {covidData.map((country) => (
           <Country
