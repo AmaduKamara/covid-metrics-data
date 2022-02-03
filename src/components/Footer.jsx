0094f07e90727ac0a00b2f1461dcd9178312f3c0
Amkam <amaduskamara@gmail.com>
2022-02-03 17:01:14 +0000

    Edit styles for footer on mobile screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <div className="py-5 flex justify-center items-center footer-hero fixed bottom-0 w-full">
-    <h1 className="font-thin text-gray-100 tracking-wide">
+    <h1 className="font-thin text-gray-100 tracking-wide text-xs md:text-lg">
       &copy; Classic 2022. All rights reserved. Developed by
       {' '}
       <a
