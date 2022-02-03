315b42b67b4e67cd6f5ce3636cdb7a729e08a6f1
Amkam <amaduskamara@gmail.com>
2022-02-03 16:42:46 +0000

    Style responsive for details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -12,7 +12,7 @@ const Details = () => {
   return (
     <div>
       <div className="py-5 hero text-white">
-        <div className="container mx-auto flex items-center justify-between">
+        <div className="container mx-auto flex items-center justify-between px-10">
           <Link to="/">
             <BiArrowBack className="text-4xl" />
           </Link>
@@ -23,30 +23,30 @@ const Details = () => {
           </div>
         </div>
       </div>
-      <div className="mt-16 mb-32 main-bg  container mx-auto">
+      <div className="mt-16 mb-32 main-bg container mx-auto px-10">
         <div className="flex justify-between items-center text-white border border-pink-400 p-8">
-          <h3 className="text-2xl font-semibold">New Comfirmed</h3>
-          <p className="text-xl">{country.NewConfirmed}</p>
+          <h3 className="text-lg md:text-2xl font-semibold">New Comfirmed</h3>
+          <p className="text-xl md:text-2xl text-gray-100">{country.NewConfirmed}</p>
         </div>
         <div className="flex justify-between items-center text-white border border-pink-400 p-8">
-          <h3 className="text-2xl font-semibold">Total Comfirmed</h3>
-          <p className="text-xl">{country.TotalConfirmed}</p>
+          <h3 className="text-lg md:text-2xl font-semibold">Total Comfirmed</h3>
+          <p className="text-xl md:text-2xl text-gray-100">{country.TotalConfirmed}</p>
         </div>
         <div className="flex justify-between items-center text-white border border-pink-400 p-8">
-          <h3 className="text-2xl font-semibold">New Death</h3>
-          <p className="text-xl">{country.NewDeaths}</p>
+          <h3 className="text-lg md:text-2xl font-semibold">New Death</h3>
+          <p className="text-xl md:text-2xl text-gray-100">{country.NewDeaths}</p>
         </div>
         <div className="flex justify-between items-center text-white border border-pink-400 p-8">
-          <h3 className="text-2xl font-semibold">Total Death</h3>
-          <p className="text-xl">{country.TotalDeaths}</p>
+          <h3 className="text-lg md:text-2xl font-semibold">Total Death</h3>
+          <p className="text-xl md:text-2xl text-gray-100">{country.TotalDeaths}</p>
         </div>
         <div className="flex justify-between items-center text-white border border-pink-400 p-8">
-          <h3 className="text-2xl font-semibold">New Recovered</h3>
-          <p className="text-xl">{country.NewRecovered}</p>
+          <h3 className="text-lg md:text-2xl font-semibold">New Recovered</h3>
+          <p className="text-xl md:text-2xl text-gray-100">{country.NewRecovered}</p>
         </div>
         <div className="flex justify-between items-center text-white border border-pink-400 p-8">
-          <h3 className="text-2xl font-semibold">Total Recovered</h3>
-          <p className="text-xl">{country.TotalRecovered}</p>
+          <h3 className="text-lg md:text-2xl font-semibold">Total Recovered</h3>
+          <p className="text-xl md:text-2xl text-gray-100">{country.TotalRecovered}</p>
         </div>
       </div>
     </div>
